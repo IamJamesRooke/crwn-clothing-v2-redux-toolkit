2800fbe6e0d7874c9d99c2fd1cb95e1437456867
IamJamesRooke <me@jamesrooke.blog>
2025-08-08 12:29:18 -0500

    ProductsContext created

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.jsx'
-import { UserProvider } from './contexts/user.contexts.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import { UserProvider } from "./contexts/user.contexts.jsx";
+import { ProductsProvider } from "./contexts/products.context.jsx";
+
+import "./index.scss";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
