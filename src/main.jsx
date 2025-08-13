5ce5cf78352b0de1d026db55ff34a13babc9005a
IamJamesRooke <me@jamesrooke.blog>
2025-08-13 15:02:58 -0500

    Getting products and categories from Firebase Store

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-import { UserProvider } from "./contexts/user.contexts.jsx";
-import { ProductsProvider } from "./contexts/products.context.jsx";
+import { UserProvider } from "./contexts/user.context.jsx";
+import { CategoriesProvider } from "./contexts/categories.context.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 
 import "./index.scss";
@@ -13,11 +13,11 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
