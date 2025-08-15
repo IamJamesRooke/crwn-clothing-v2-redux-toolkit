d7823441e49c8205a02b6358245a67b81acebfdb
IamJamesRooke <me@jamesrooke.blog>
2025-08-15 13:02:32 -0500

    button styled components migration complete

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,5 +1,6 @@
+import styled from "styled-components";
 
-.button-container {
+export const BaseButton = styled.button`
     min-width: 165px;
     width: auto;
     height: 50px;
@@ -22,18 +23,19 @@
         color: black;
         border: 1px solid black;
     }
-
-    &.google-sign-in {
-        background-color: #4285f4;
-        color: white;
-
-        &:hover {
-            background-color: #357ae8;
-            border: none;
-        }
+`;
+    
+export const GoogleSignInButton = styled(BaseButton)`
+    background-color: #4285f4;
+    color: white;
+    
+    &:hover {
+        background-color: #357ae8;
+        border: none;
     }
+`;
 
-    &.inverted {
+export const InvertedButton = styled(BaseButton)`
         background-color: white;
         color: black;
         border: 1px solid black;
@@ -43,5 +45,4 @@
             color: white;
             border: none;
         }
-    }
-}
+`;
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import { 
     signInWithGooglePopup,
@@ -71,7 +71,13 @@ const SignInForm = () => {
 
                 <ButtonsContainer>
                     <Button type="submit">Sign In</Button>
-                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                    <Button 
+                        buttonType={BUTTON_TYPE_CLASSES.google}
+                        type='button'
+                        onClick={signInWithGoogle}
+                    >
+                        Google Sign In
+                    </Button>
                 </ButtonsContainer>
             </form>
         </SignInContainer>
