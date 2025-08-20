48544f50c4960541f8c95a724b59302b8c70a8a2
IamJamesRooke <me@jamesrooke.blog>
2025-08-20 15:06:42 -0500

    migration from context to redux for cart complete

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,9 @@
-import { useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector'
+import { setIsCartOpen } from '../../store/cart/cart.action'
 
 import shoppingIcon from '../../assets/shopping-bag.svg'
-
-import { CartContext } from '../../contexts/cart.context';
 
 import { 
   CartIconContainer, 
@@ -11,10 +12,12 @@ import {
 } from './cart-icon.styles';
 
 const CartIcon = () => {
-    
-    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+    const dispatch = useDispatch();
 
-    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    const cartCount = useSelector(selectCartCount);
+    const isCartOpen = useSelector(selectIsCartOpen);
+
+    const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
     return (
         <CartIconContainer onClick={toggleIsCartOpen}>
