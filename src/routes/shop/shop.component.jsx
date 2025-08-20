1117481ed4e4d5ff536d8f8504a43aa3fe27620e
IamJamesRooke <me@jamesrooke.blog>
2025-08-20 11:52:15 -0500

    business logic of category selector implemented

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../../components/category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = () => {
 
@@ -16,8 +17,8 @@ const Shop = () => {
           // This piece of code only for initial upload
           // await addCollectionAndDocuments('categories', SHOP_DATA);
           
-          const categoryMap = await getCategoriesAndDocuments('categories');
-          dispatch(setCategoriesMap(categoryMap));
+          const categoriesArray = await getCategoriesAndDocuments('categories');
+          dispatch(setCategories(categoriesArray));
       };
 
       getCategoriesMap();
