788c539054ead57e45415032c193bf1ef7b467c0
IamJamesRooke <me@jamesrooke.blog>
2025-08-25 14:46:23 -0500

    redux devtools setup and logger middleware refactor

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
@@ -14,14 +13,19 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [logger]
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+
+const composeEnhancer = 
+    (process.env.NODE_ENV !== 'production' && 
+        window && 
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
+    compose;
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
-
 
 export const store = createStore(
     persistedReducer, 
     undefined, 
-    composedEnhancers)
+    composedEnhancers);
 
 export const persistor = persistStore(store);
